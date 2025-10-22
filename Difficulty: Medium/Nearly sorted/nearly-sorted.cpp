794f02c81c7b55e114dcf1ce1cb2804a63bf9f1a
Soriful Islam Sk <sorifulislamsk1@gmail.com>
2025-10-22 22:01:16 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Nearly sorted/nearly-sorted.cpp
@@ -0,0 +1,22 @@
+class Solution {
+  public:
+    void nearlySorted(vector<int>& arr, int k) {
+        // code here
+        priority_queue<int,vector<int>, greater<int>>pq;
+        
+        for(int i=0;i<=k;i++){
+            pq.push(arr[i]);
+        }
+        int index=0;
+        for(int i=k+1;i<arr.size();i++){
+            arr[index++]=pq.top();
+            pq.pop();
+            pq.push(arr[i]);
+        }
+        
+        while(!pq.empty()){
+            arr[index++] = pq.top();
+            pq.pop();
+        }
+    }
+};
