0c94977ca7ddc98755e2a04202cfb29a62730889
Soriful Islam Sk <sorifulislamsk1@gmail.com>
2025-11-10 22:12:07 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Stock Buy and Sell with Cooldown/stock-buy-and-sell-with-cooldown.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& arr) {
+        int n = arr.size();
+        if (n == 0) return 0;
+
+        vector<int> buy(n, 0), sell(n, 0), cool(n, 0);
+
+        buy[0] = -arr[0];
+        sell[0] = 0;
+        cool[0] = 0;
+
+        for (int i = 1; i < n; i++) {
+            buy[i] = max(buy[i - 1], cool[i - 1] - arr[i]);
+            sell[i] = max(sell[i - 1], buy[i - 1] + arr[i]);
+            cool[i] = max(cool[i - 1], sell[i - 1]);
+        }
+
+        return max(sell[n - 1], cool[n - 1]);
+    }
+};
