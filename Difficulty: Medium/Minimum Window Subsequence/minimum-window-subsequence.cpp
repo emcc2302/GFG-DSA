6e5b819338fe5b98ac78098348d0d3c932d433a8
Soriful Islam Sk <sorifulislamsk1@gmail.com>
2026-01-11 21:41:41 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Minimum Window Subsequence/minimum-window-subsequence.cpp
@@ -0,0 +1,43 @@
+class Solution {
+  public:
+    string minWindow(string& s1, string& s2) {
+        int n = s1.size();
+        int m = s2.size();
+        
+        int minLen = INT_MAX;
+        int start = -1;
+        
+        int i = 0, j = 0;
+        
+        while (i < n) {
+            if (s1[i] == s2[j]) {
+                j++;
+            }
+            
+            if (j == m) {
+                int end = i;
+                
+                j--;
+                while (j >= 0) {
+                    if (s1[i] == s2[j]) {
+                        j--;
+                    }
+                    i--;
+                }
+                
+                i++; 
+                j++;
+                
+                if (end - i + 1 < minLen) {
+                    minLen = end - i + 1;
+                    start = i;
+                }
+            }
+            
+            i++;
+        }
+        
+        if (start == -1) return "";
+        return s1.substr(start, minLen);
+    }
+};
