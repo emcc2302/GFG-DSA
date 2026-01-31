226580467fb426f55be9e981aaebb241c5b5b1dd
Soriful Islam Sk <sorifulislamsk1@gmail.com>
2026-01-31 21:25:53 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Implement k Queues in a Single Array/implement-k-queues-in-a-single-array.cpp
@@ -0,0 +1,67 @@
+class kQueues {
+    int n, k;
+    int *arr, *front, *rear, *next;
+    int freeSpot;
+
+public:
+    kQueues(int n, int k) {
+        this->n = n;
+        this->k = k;
+
+        arr = new int[n];
+        front = new int[k];
+        rear = new int[k];
+        next = new int[n];
+
+        for (int i = 0; i < k; i++) {
+            front[i] = -1;
+            rear[i] = -1;
+        }
+
+        for (int i = 0; i < n - 1; i++) {
+            next[i] = i + 1;
+        }
+        next[n - 1] = -1;
+
+        freeSpot = 0;
+    }
+
+    void enqueue(int x, int i) {
+        if (isFull()) return;
+
+        int index = freeSpot;
+        freeSpot = next[index];
+
+        if (front[i] == -1) {
+            front[i] = index;
+        } else {
+            next[rear[i]] = index;
+        }
+
+        next[index] = -1;
+        rear[i] = index;
+        arr[index] = x;
+    }
+
+    int dequeue(int i) {
+        if (isEmpty(i)) return -1;
+
+        int index = front[i];
+        front[i] = next[index];
+
+        if (front[i] == -1) rear[i] = -1;
+
+        next[index] = freeSpot;
+        freeSpot = index;
+
+        return arr[index];
+    }
+
+    bool isEmpty(int i) {
+        return front[i] == -1;
+    }
+
+    bool isFull() {
+        return freeSpot == -1;
+    }
+};
