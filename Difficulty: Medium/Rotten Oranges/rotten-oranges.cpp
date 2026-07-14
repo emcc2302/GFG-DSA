6ae55f27259909a0ae7348a6229946b39cbbdf1f
Soriful Islam Sk <sorifulislamsk1@gmail.com>
2026-07-14 09:01:28 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Rotten Oranges/rotten-oranges.cpp
+++ Difficulty: Medium/Rotten Oranges/rotten-oranges.cpp
@@ -1,59 +1,58 @@
 class Solution {
   public:
-    int orangesRot(vector<vector<int>>& mat) {
+    int orangesRot(vector<vector<int>>& grid ) {
         // code here
-        int n = mat.size(), m = mat[0].size();
-        queue<pair<pair<int,int>,int>> q;
-        int vis[n][m];
-        
-        for(int i = 0; i<n; i++){
-            for(int j = 0; j<m ; j++){
-                if(mat[i][j]==2){
-                    q.push({{i,j},0});
+         int n = grid.size();
+        int m = grid[0].size();
+    //we push in the queue {{row,col},time}
+    //initially time will be 0 and then inc
+        queue<pair<pair<int, int>, int>> q;
+        vector<vector<int>> vis(n, vector<int>(m, 0));
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == 2) {
+                    q.push({{i, j}, 0});
                     vis[i][j] = 2;
                 }
                 else{
-                    vis[i][j] = 0;
+                    vis[i][j]=0;
                 }
             }
         }
-        
-        int drow[] = {1,0,-1,0}; //delta row which will be the neighbour
-        int dcol[] = {0,1,0,-1};
-        
+
         int tm = 0;
-        
-        while(!q.empty()){
-            int r = q.front().first.first; //row h
-            int c = q.front().first.second; // colm 
-            int t = q.front().second; //time hai
+        int delRow[] = {-1, 0, 1, 0};
+        int delCol[] = {0, 1, 0, -1};
+
+        while (!q.empty()) {
+            int row = q.front().first.first;
+            int col = q.front().first.second;
+            int t = q.front().second;
             q.pop();
-            
-            tm = max(tm,t);
-            
-            for(int i = 0; i<4 ; i++){
-                int nrow = r + drow[i];
-                int ncol = c + dcol[i]; //neighbouring row of curr idx
-                 
-                if(isSafe(nrow, ncol, n, m) && vis[nrow][ncol] != 2 && mat[nrow][ncol] == 1){
-                    q.push({{nrow, ncol}, t+1});
+
+            tm = max(tm, t);
+
+            for (int i = 0; i < 4; i++) {
+                 int nrow=row+delRow[i];
+                int ncol=col+delCol[i];
+
+                if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m &&
+                    grid[nrow][ncol] == 1 && vis[nrow][ncol] != 2) {
                     vis[nrow][ncol] = 2;
+                    q.push({{nrow, ncol}, t + 1});
                 }
             }
         }
-        for(int i = 0; i<n ; i++){
-            for(int j = 0; j<m ; j++){
-                if(vis[i][j]!=2 && mat[i][j] == 1 ){
+        // Check if any fresh orange remains
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+
+                if (grid[i][j] == 1 && vis[i][j] != 2)
                     return -1;
-                }
             }
         }
+
         return tm;
     }
-    
-    
-    private:
-      bool isSafe(int r, int c, int n, int m){
-          return (r >= 0 && r<n && c >=0 && c<m);
-      }
 };
